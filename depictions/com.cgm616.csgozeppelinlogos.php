b763dc43c833f7b94dd68ccbbf234924a1bd3f03
Cole Graber-Mitchell <cgm616@me.com>
2015-02-15 11:32:01 -0600

    updated csgozl depiction

--- depictions/com.cgm616.csgozeppelinlogos.php
+++ depictions/com.cgm616.csgozeppelinlogos.php
@@ -1,8 +1,8 @@
 <?php
     $name="CSGOZeppelinLogos";
     $description=array("Some Zeppelin logos related to CSGO.  Has the Valve logo, the CT logo, the T logo, the Steam logo, and the CSGO logo/banner.",
-    "If you'd like to design me an icon, please do so!",
-    "All logos are copyright Valve and it’s subsidiaries.  All I did was resize them.");
+    "If you'd like to design me a logo, please do so!",
+    "All logos are copyright Valve and it’s subsidiaries.  All I/Madhav did was resize them.");
     
     $changelog=array("0.0.1-1", " - Testing depictions.",
     "0.0.2-1", " - More depictions stuff.");
