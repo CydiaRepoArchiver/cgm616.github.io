8deeff50e244932358e07b544a864c43dea36ef5
Cole Graber-Mitchell <cgm616@me.com>
2015-02-15 10:55:21 -0600

    made depictions work, not through github pages though

--- depictions/com.cgm616.csgozeppelinlogos.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $name=“CSGO Zeppelin Logos“;
-    $description=array(“A few logos for Zeppelin related to csgo.”,
-    “Right now it is just a test package with nothing in it, but coming soon!”);
-?>
---
+++ depictions/csgozeppelinlogos.php
@@ -0,0 +1,11 @@
+<?php
+    $name="CSGOZeppelinLogos";
+    $description=array("Some Zeppelin logos related to CSGO.  Has the Valve logo, the CT logo, the T logo, the Steam logo, and the CSGO logo/banner.",
+    "If you'd like to design me an icon, please do so!",
+    "All logos are copyright Valve and it’s subsidiaries.  All I did was resize them.");
+    
+    $changelog=array("0.0.1-1", " - Testing depictions.");
+    
+    $devText="Developer's Twitter";
+    $devLink="https://twitter.com/JamesIscNeutron";
+?>
