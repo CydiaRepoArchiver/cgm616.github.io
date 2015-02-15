72ee63adc81f24be3bf6367c3af02ccce5f4421e
Cole Graber-Mitchell <cgm616@me.com>
2015-02-15 09:38:34 -0600

    updated csgozeppelinlogos

--- stylesheets/com.cgm616.csgozeppelinlogos.php
+++ stylesheets/com.cgm616.csgozeppelinlogos.php
@@ -2,4 +2,9 @@
     $name=“CSGO Zeppelin Logos“;
     $description=array(“A few logos for Zeppelin related to csgo.”,
     “Right now it is just a test package with nothing in it, but coming soon!”);
+    
+    $changelog=array(“0.0.1-1”, " - Initial release.");
+    
+    $devText=“Madhav’s Email”;
+    $devLink=“mailto://madhav908+cydia@gmail.com”;
 ?>
