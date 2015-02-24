9ffd2f1960162a268f7e5b32c17eaf885a53f8cc
Cole Graber-Mitchell <cgm616@me.com>
2015-02-23 21:18:34 -0600

    updated dpkg-scanpackages

--- repo/push.sh
+++ repo/push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # Pushes DEBs to the Packages file and bzips it up; all I need to do is push to GitHub after this
 
-dpkg-scanpackages debs / > Packages
+dpkg-scanpackages debs /dev/null > Packages # Uses dpkg-scanpackages to create Packages file with deb info
 
-bzip2 -fks Packages
+bzip2 -fks Packages # Puts Packages into a bzip2
