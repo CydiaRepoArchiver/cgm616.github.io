c49e249089df91a441bdcd60bb026ed7d83b748e
Cole Graber-Mitchell <cgm616@me.com>
2015-03-14 14:20:54 -0500

    Fixed up some formatting to look better

--- repo/depictions/com.cgm616.csgozeppelinlogos.php
+++ repo/depictions/com.cgm616.csgozeppelinlogos.php
@@ -1,9 +1,9 @@
 <?php
     $name="CSGOZeppelinLogos";
     $description=array("Some Zeppelin logos related to CSGO.  Has the Valve logo, the CT logo, the T logo, the Steam logo, and the CSGO logo/banner.",
-    "If you'd like to design me a logo, please do so!",
-    "All logos are copyright Valve and it’s subsidiaries.  All I/Madhav did was resize them.");
-    
+                       "If you'd like to design me a logo, please do so!",
+                       "All logos are copyright Valve and it’s subsidiaries.  All I/Madhav did was resize them.");
+
     $changelog=array("1.1.1-1", " - Fixed versioning error to allow package updating.",
                      "1.1.0-1", " - Resized CT and T logos to look better.",
                      "1.0.2-1", " - Fixed including .DS_Store files.",
@@ -12,7 +12,7 @@
                      "0.0.3-1", " - Trying to fix package install errors.",
                      "0.0.2-1", " - More depictions stuff.",
                      "0.0.1-1", " - Testing depictions.");
-    
+
     $devText="Package Contents on Github";
     $devLink="https://github.com/cgm616/csgozeppelinlogos";
 ?>
