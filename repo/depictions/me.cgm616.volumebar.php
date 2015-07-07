ef9b0e25ef634ffa79a115e4925f8320ce4b121e
cgm616 <cgm616@me.com>
2015-07-07 09:13:28 -0500

    Add VolumeBar v0.0.1~beta
    Add deb, update package files, and add depiction and screenshots for VolumeBar.

---
+++ repo/depictions/me.cgm616.volumebar.php
@@ -0,0 +1,11 @@
+<?php
+    $name="VolumeBar";
+    $description=array("Removes the ugly volume HUD and changes it to a bar like a notification.  The bar has a slider which can also be used to changed the volume.",
+                       "This package is still in beta.  Please expect bugs.  Send any bugs you find to my email address.",
+                       "Known bug: when changing the ringer volume, slider does not move");
+
+    $changelog=array("0.0.1~beta", " - Beta release");
+
+    $devText="Package Contents on Github";
+    $devLink="https://github.com/cgm616/volumebar";
+?>
