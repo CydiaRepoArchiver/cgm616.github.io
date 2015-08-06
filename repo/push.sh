efcc4014612c66f577676e73ccea94c7b14454c6
cgm616 <cgm616@me.com>
2015-08-05 22:16:02 -0500

    Add support for downgrading packages
    Change push.sh to use the --multiversion option for dpkg-scanpackages. Add past versions
    of VolumeBar to allow downgrading.

--- repo/push.sh
+++ repo/push.sh
@@ -2,7 +2,7 @@
 
 # Pushes DEBs to the Packages file and bzips it up; all I need to do is push to GitHub after this
 
-dpkg-scanpackages debs /dev/null > Packages # Uses dpkg-scanpackages to create Packages file with deb info
+dpkg-scanpackages -m debs /dev/null > Packages # Uses dpkg-scanpackages to create Packages file with deb info
 
 bzip2 -fks Packages # Puts Packages into a bzip2
 
