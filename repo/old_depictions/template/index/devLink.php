9d0a37b11559103a573db764d8271cff21058fa3
cgm616 <cgm616@me.com>
2016-08-29 14:21:00 -0500

    Update VB9 depiction, remove unneeded old depictions

--- repo/old_depictions/template/index/devLink.php
+++
@@ -1,4 +0,0 @@
-<ul>
-<li>
-<a class=\"devLink\" target=\"_blank\" href="https://github.com/cgm616/cgm616.github.io">cgm616's repo on Github</a></li>
-<li>
--- repo/old_depictions/template/index/footer.php
+++
@@ -1,3 +0,0 @@
-<p><a href="http://cgm616.github.io” target="_blank">cgm616’s repo</a></p>
-</body>
-</html>
